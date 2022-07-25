6d6189ab0ab8c4b60e254a5067c109c3104df4e8
Nuzhat Minhaz <60846613+nuzhat-minhaz@users.noreply.github.com>
2022-07-25 01:03:12 -0700

    nuxt 3 starter styled

---
+++ app.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="home-page">
+      <h1>Hi! This is a simple Nuxt 3 app.</h1>
+      <h2>Click this button below to check out a server route :) </h2>
+      <NuxtLink to="/hello" target="_blank">
+        <button> 
+            What Time Is It?
+        </button>
+      </NuxtLink>
+      <h2> Deploy a server-rendered Nuxt 3 app on Azure Static Web Apps using <NuxtLink to="#" class="styling">the Microsoft documentation</NuxtLink><br>
+       OR check out this <NuxtLink to="#" class="styling">blogpost</NuxtLink> about Nuxt 3 support on Azure Static Web Apps.</h2>
+    </div>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<style>
+  .home-page{
+    margin: 5px;
+    margin-bottom: 0;
+    padding: 20px;
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+
+  .styling{
+    color: rgb(29, 199, 128);
+    font-weight: 500;
+    text-decoration: none;
+  }
+
+  .styling:hover{
+    cursor: pointer;
+    color: rgb(18, 168, 106);
+    font-weight: 500;
+    text-decoration: underline;
+    transition: 0.2s ease-in-out;
+
+  }
+
+  .home-page h1{
+    font-family: ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,"Apple Color Emoji","Segoe UI Emoji",Segoe UI Symbol,"Noto Color Emoji";
+    font-weight: 500;
+    font-size: 1.5rem;
+    line-height: 2rem;
+  }
+
+  .home-page h2{
+    font-family: ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,"Apple Color Emoji","Segoe UI Emoji",Segoe UI Symbol,"Noto Color Emoji";
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 2rem;
+  }
+
+  button {
+    font-family: ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,"Apple Color Emoji","Segoe UI Emoji",Segoe UI Symbol,"Noto Color Emoji";
+    font-weight: 500;
+    font-size: 1.5rem;
+    line-height: 2rem;
+    color: white;
+    background-color: rgb(42, 219, 145);
+    padding: 10px 30px 10px 30px;
+    margin: 0px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border: none;
+    border-radius: 5px;
+    font-size: 1rem;
+  }
+
+  button:hover{
+    cursor: pointer;
+    background-color: rgb(29, 199, 128);
+    transition: 0.2s ease-in;
+  }
+</style>
